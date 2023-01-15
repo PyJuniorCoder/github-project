16bae0f6a920cf85df620ea48cab3c78f755cb8d
Dilmuxammad <dilmuxammad1593adda@gmail.com>
2023-01-15 14:35:30 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+"use strict";  // qat'iy kod nazorati 
+
+// const place = confirm("Are you here");
+// console.log(place);
+
+
+// const JPlace = prompt ("Where are you?","Defalt qiymat"); 
+
+// let incr = 10;
+// let decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr, decr);
+
+
+console.log(5*5 != 25);
+console.log(5*5 == "25");
+console.log(5*5 === 25);
